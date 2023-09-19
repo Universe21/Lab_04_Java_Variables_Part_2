4ec4af4eaf863b5dff458c9092eb221f086cbd3b
Universe21 <leachhs@mail.uc.edu>
2023-09-18 23:11:12 -0400

    Project into

---
+++ src/com/company/Main.java
@@ -0,0 +1,34 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // the cost of my lunch:
+        double myLunchCost = 12.50;
+        // the number of kids in your family:
+         int numberOfKidsInFamily = 2;
+        // the answer to is it raining? :
+         boolean isItRaining = false;
+        // the price of a gallon of gas :
+        double priceOfGallonOfGas = 2.99;
+        // your favorite number :
+         int favoriteNumber = 12;
+        // your shoe size:
+        double shoeSize = 9.5;
+        // your birth month:
+        String birthMonth = "April";
+        // your full name:
+         String fullName = "Heylee Leach";
+
+            System.out.println("The cost of my lunch: " + myLunchCost);
+            System.out.println("The number of kids in my family: " + numberOfKidsInFamily);
+            System.out.println("Is it raining?: " + isItRaining);
+            System.out.println("The price of a gallon of gas: " + priceOfGallonOfGas);
+            System.out.println("My favorite number: " + favoriteNumber);
+            System.out.println("My shoe size: " + shoeSize);
+            System.out.println("My birth month: " + birthMonth);
+            System.out.println("My full name: " + fullName);
+
+    }
+}
